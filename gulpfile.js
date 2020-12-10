474d80a8317ffbea3eea9e3221497fb3799192d2
mlyubimov <mlyubimov.web@gmail.com>
2020-12-10 03:35:07 +0300

    Updated gulp, added storage and its connection, added the public folder

--- gulpfile.js
+++ gulpfile.js
@@ -5,11 +5,13 @@ const concat = require('gulp-concat');
 const autoprefixer = require('gulp-autoprefixer');
 const cleanCSS = require('gulp-clean-css');
 const minify = require('gulp-minify');
+const jsonMinify = require('gulp-json-minify');
 const del = require('del');
 const browserSync = require('browser-sync').create();
 
 const sourcemaps = require('gulp-sourcemaps');
 const sass = require('gulp-sass');
+const { watch } = require('browser-sync');
 
 // Порядок подключения CSS
 const cssFiles = [
@@ -20,9 +22,21 @@ const cssFiles = [
 	'./src/scss/widget.scss'
 ]
 
-function styles() {
+// Вызов функции public
+gulp.task('public', function () {
+	// Шаблон для поиска публичных файлов
+	return gulp.src('./public/*')
+
+	// Выходная папка для публичных файлов
+	.pipe(gulp.dest('./dist'))
+
+	// Синхронизирует браузер
+	.pipe(browserSync.stream());
+})
+
+// Вызов функции styles
+gulp.task('styles', function () {
 	// Шаблон для поиска файлов CSS
-	// Все файлы по шаблону './src/css/**/*.css'
 	return gulp.src(cssFiles)
 
 	.pipe(sourcemaps.init())
@@ -49,14 +63,15 @@ function styles() {
 
 	// Синхронизирует браузер
 	.pipe(browserSync.stream());
-}
+})
 
-function scripts() {
+// Вызов функции scripts
+gulp.task('scripts', function () {
 	// Шаблон для поиска файлов js
 	return gulp.src('./src/js/*.js')
 
 	.pipe(sourcemaps.init())
-    .pipe(babel())
+	.pipe(babel())
 
 	// Конкотинация или соединение
 	.pipe(concat('app.js'))
@@ -78,13 +93,26 @@ function scripts() {
 
 	// Синхронизирует браузер
 	.pipe(browserSync.stream());
-}
+})
 
-// Вызов функции styles
-gulp.task('styles', styles);
+// Вызов функции storage
+gulp.task('storage', function () {
+	// Шаблон для поиска файлов json
+	return gulp.src('./src/storage/*.json')
 
-// Вызов функции scripts
-gulp.task('scripts', scripts);
+	.pipe(sourcemaps.init())
+
+	// Минификация JSON
+	.pipe(jsonMinify())
+
+	.pipe(sourcemaps.write("."))
+
+	// Выходная папка для скриптов
+	.pipe(gulp.dest('./dist/storage'))
+
+	// Синхронизирует браузер
+	.pipe(browserSync.stream());
+})
 
 // Копирование фотографий и шрифтов
 gulp.task('copy', function() {
@@ -99,31 +127,33 @@ gulp.task('copy', function() {
 
 // Вызов функции clean
 gulp.task('del', function(done) {
-	del.sync(['dist/css', 'dist/js'])
+	del.sync('dist/*')
 	done();
 });
 
 // Вызов функции clean
-gulp.task('watch', function() {
+gulp.task('watch', function(cb) {
 	// Обновляет браузер
 	browserSync.init({
 		server: {
-				baseDir: "./dist"
+			baseDir: "./dist"
 		}
 	});
+
 	// Следит за CSS файлами
-	gulp.watch('./src/scss/**/*.css', styles)
-	gulp.watch('./src/scss/**/*.scss', styles)
-	gulp.watch('./src/scss/**/*.sass', styles)
+	gulp.watch(['./src/scss/**/*.css', './src/scss/**/*.scss', './src/scss/**/*.sass'], gulp.task('styles'))
 	// Следит за JS файлами
-	gulp.watch('./src/js/**/*.js', scripts)
-	// При изменении HTML запускает синхронизацию
-	gulp.watch("./dist/*.html").on('change', browserSync.reload);
+	gulp.watch('./src/js/**/*.js', gulp.task('scripts'))
+	// Следит за JSON файлами
+	gulp.watch('./src/storage/**/*.json', gulp.task('storage'))
+	// Следит за HTML файлами
+	gulp.watch("./dist/*.html").on('change', gulp.task('public'));
 });
 
 // Вызов сборщика
-gulp.task('build', gulp.series('del', gulp.parallel('styles', 'scripts', 'copy')));
+gulp.task('build', gulp.series('del', gulp.parallel('public', 'styles', 'scripts', 'storage', 'copy')));
 
+// Вызов developer-mode
 gulp.task('dev', gulp.series('build', 'watch'));
 
 // Вызов происходит в консоли с помощью gulp {название таска}
